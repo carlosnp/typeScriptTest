2bd17138406fc7fa9c0cdbd5c657ba4f2d828fa7
Carlos Nava <machinery.e@gmail.com>
2020-09-17 15:16:12 -0400

    merge

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,36 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+/*
+* 1) No se puede cambiar el tipo de variable
+* 2) Una vez definida un objeto no se le pueden agregar propiedades
+* 3) No se puede cambiar la estructura de un objeto
+*/
+// Characters, number and boolean
+// let character = 'mario';
+// let age = 36;
+// let isSome = true;
+// console.log(character, age, isSome);
+// Arrays
+var names = ['Lawson', 'Cedrick', 'Eldora', 'Dewayne'];
+names.push('Destini');
+var mixed = ['Reilly', 'Karianne', 'Candido', 1, 2, 3];
+mixed.push(10);
+mixed.push('Kacey');
+console.log('names', names);
+console.log('mixed', mixed);
+// Objects
+var obj = {
+    name: 'Rosella',
+    color: 'maine_buckinghamshire.blb',
+    age: 36
+};
+obj.name = 'Vella';
+console.log('obj', obj);
+/*
+ const inputs = document.querySelectorAll('input');
+ inputs.forEach(input => {
+   console.log(input);
+ });
+ */
+//  const circ = (diameter:number)=>{
+//    return diameter * Math.PI;
+//  }
+//  console.log(`Circ= ${circ(3)}`);
