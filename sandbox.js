0be8f2fba399d5ae76ae05e43e0e51d5467083dd
Carlos Nava <machinery.e@gmail.com>
2020-09-17 15:31:36 -0400

    Tipos de variables explicitos

--- sandbox.js
+++ sandbox.js
@@ -3,27 +3,12 @@
 * 2) Una vez definida un objeto no se le pueden agregar propiedades
 * 3) No se puede cambiar la estructura de un objeto
 */
+// =======================================================
 // Characters, number and boolean
 // let character = 'mario';
 // let age = 36;
 // let isSome = true;
 // console.log(character, age, isSome);
-// Arrays
-var names = ['Lawson', 'Cedrick', 'Eldora', 'Dewayne'];
-names.push('Destini');
-var mixed = ['Reilly', 'Karianne', 'Candido', 1, 2, 3];
-mixed.push(10);
-mixed.push('Kacey');
-console.log('names', names);
-console.log('mixed', mixed);
-// Objects
-var obj = {
-    name: 'Rosella',
-    color: 'maine_buckinghamshire.blb',
-    age: 36
-};
-obj.name = 'Vella';
-console.log('obj', obj);
 /*
  const inputs = document.querySelectorAll('input');
  inputs.forEach(input => {
@@ -34,3 +19,53 @@ console.log('obj', obj);
 //    return diameter * Math.PI;
 //  }
 //  console.log(`Circ= ${circ(3)}`);
+// =======================================================
+// Arrays
+// let names =['Lawson','Cedrick','Eldora','Dewayne']
+// names.push('Destini');
+// let mixed = ['Reilly','Karianne','Candido',1,2,3];
+// mixed.push(10);
+// mixed.push('Kacey')
+// console.log('names', names);
+// console.log('mixed', mixed);
+// Objects
+// let obj ={
+//   name:'Rosella',
+//   color:'maine_buckinghamshire.blb',
+//   age:36
+// }
+// obj.name = 'Vella';
+// console.log('obj', obj);
+// =======================================================
+// Explicit Types
+var char;
+var age;
+var isLogin;
+char = 'Meghan Rodriguez';
+age = 654780865;
+isLogin = false;
+// Array of string
+var objS = [];
+/*
+ * Se iguala a [] para que por defecto sea un array vacio
+ * y se pueda pushear elementos en el array
+ */
+objS.push('MacGyver');
+console.log('obj', objS);
+// Many types
+var mixed = [];
+mixed.push('Malachi Collier Sr.');
+mixed.push(3);
+mixed.push(true);
+console.log('mixed', mixed);
+var uid;
+uid = 123;
+uid = 'Regional Web Producer';
+console.log('uid', uid);
+// Object
+var obj1;
+obj1 = { name: 'Ernser', age: 9 };
+console.log('obj1', obj1);
+var obj2;
+obj2 = { name: 'Gillian Donnelly', age: 93, color: 'Research' };
+console.log('obj2', obj2);
--- sandbox.ts
+++ sandbox.ts
@@ -4,33 +4,12 @@
 * 3) No se puede cambiar la estructura de un objeto
 */
 
+// =======================================================
 // Characters, number and boolean
 // let character = 'mario';
 // let age = 36;
 // let isSome = true;
 // console.log(character, age, isSome);
-
-// Arrays
-let names =['Lawson','Cedrick','Eldora','Dewayne']
-names.push('Destini');
-
-let mixed = ['Reilly','Karianne','Candido',1,2,3];
-mixed.push(10);
-mixed.push('Kacey')
-
-console.log('names', names);
-console.log('mixed', mixed);
-
-// Objects
-
-let obj ={
-  name:'Rosella',
-  color:'maine_buckinghamshire.blb',
-  age:36
-}
-obj.name = 'Vella';
-
-console.log('obj', obj);
 
 /* 
  const inputs = document.querySelectorAll('input');
@@ -43,3 +22,72 @@ console.log('obj', obj);
 //    return diameter * Math.PI;
 //  }
 //  console.log(`Circ= ${circ(3)}`);
+
+// =======================================================
+// Arrays
+// let names =['Lawson','Cedrick','Eldora','Dewayne']
+// names.push('Destini');
+
+// let mixed = ['Reilly','Karianne','Candido',1,2,3];
+// mixed.push(10);
+// mixed.push('Kacey')
+
+// console.log('names', names);
+// console.log('mixed', mixed);
+
+// Objects
+
+// let obj ={
+//   name:'Rosella',
+//   color:'maine_buckinghamshire.blb',
+//   age:36
+// }
+// obj.name = 'Vella';
+
+// console.log('obj', obj);
+
+// =======================================================
+// Explicit Types
+
+let char:string;
+let age:number;
+let isLogin:boolean;
+
+char='Meghan Rodriguez';
+age=654780865;
+isLogin=false;
+
+// Array of string
+let objS: string[] = [] 
+/*
+ * Se iguala a [] para que por defecto sea un array vacio 
+ * y se pueda pushear elementos en el array
+ */ 
+objS.push('MacGyver')
+console.log('obj', objS);
+
+// Many types
+let mixed: (string|number|boolean)[]=[];
+mixed.push('Malachi Collier Sr.')
+mixed.push(3)
+mixed.push(true)
+console.log('mixed', mixed);
+
+let uid:string|number;
+uid = 123;
+uid = 'Regional Web Producer' 
+console.log('uid', uid);
+
+// Object
+let obj1:object;
+obj1={name:'Ernser', age:9}
+console.log('obj1', obj1);
+
+let obj2:{
+  name:string,
+  age:number,
+  color:string,
+}
+
+obj2={name:'Gillian Donnelly', age:93,color:'Research'}
+console.log('obj2', obj2);
