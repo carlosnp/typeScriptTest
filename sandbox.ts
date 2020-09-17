c490a0f2226f15041dda02467cff349fca34a5e6
Carlos Nava <machinery.e@gmail.com>
2020-09-17 15:15:16 -0400

    arrays and objects

--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,36 @@
 /*
-* No se puede cambiar el tipo de variable
+* 1) No se puede cambiar el tipo de variable
+* 2) Una vez definida un objeto no se le pueden agregar propiedades
+* 3) No se puede cambiar la estructura de un objeto
 */
 
-let character = 'mario';
-let age = 36;
-let isSome = true;
+// Characters, number and boolean
+// let character = 'mario';
+// let age = 36;
+// let isSome = true;
+// console.log(character, age, isSome);
 
-console.log(character, age, isSome);
+// Arrays
+let names =['Lawson','Cedrick','Eldora','Dewayne']
+names.push('Destini');
+
+let mixed = ['Reilly','Karianne','Candido',1,2,3];
+mixed.push(10);
+mixed.push('Kacey')
+
+console.log('names', names);
+console.log('mixed', mixed);
+
+// Objects
+
+let obj ={
+  name:'Rosella',
+  color:'maine_buckinghamshire.blb',
+  age:36
+}
+obj.name = 'Vella';
+
+console.log('obj', obj);
 
 /* 
  const inputs = document.querySelectorAll('input');
@@ -15,7 +39,7 @@ console.log(character, age, isSome);
  });
  */ 
 
- const circ = (diameter:number)=>{
-   return diameter * Math.PI;
- }
- console.log(`Circ= ${circ(3)}`);
+//  const circ = (diameter:number)=>{
+//    return diameter * Math.PI;
+//  }
+//  console.log(`Circ= ${circ(3)}`);
