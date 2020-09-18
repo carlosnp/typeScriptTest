4cf1c2a27f2b632720a744bac19d6ff6934ab19d
Carlos Nava <machinery.e@gmail.com>
2020-09-18 10:31:27 -0400

    se altera el config

--- public/app.js
+++ public/app.js
@@ -1,2 +1,8 @@
 "use strict";
 console.log("%c APP", ColorsLog2);
+var anchor = document.querySelector('a');
+if (anchor) {
+    console.log('anchor', anchor);
+}
+var form = document.querySelector('#new-item-form');
+console.log('form', form.children);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
 console.log(`%c APP`,ColorsLog2);
+
+const anchor = document.querySelector('a');
+if (anchor) {
+  console.log('anchor', anchor);
+}
+
+const form = document.querySelector('#new-item-form') as HTMLFontElement;
+console.log('item-form', form.children);
