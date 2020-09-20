8e6ad1dfc708c87394293c2e3020df6f5a6f15dc
Carlos Nava <machinery.e@gmail.com>
2020-09-20 08:18:19 -0400

    se agrega el editConfig

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,13 @@ const ColorsLog2 = [
 
 console.log(`%c APP`,ColorsLog2);
 
-const anchor = document.querySelector('a');
-if (anchor) {
-  console.log('anchor', anchor);
-}
+// const anchor = document.querySelector('a');
+// if (anchor) {
+//   console.log('anchor', anchor);
+// }
 
 const form = document.querySelector('#new-item-form') as HTMLFontElement;
-console.log('item-form', form.children);
+// console.log('item-form', form.children);
 
 const type    = document.querySelector('#type') as HTMLSelectElement;
 const tofrom  = document.querySelector('#tofrom') as HTMLInputElement;
