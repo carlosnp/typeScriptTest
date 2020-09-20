ff27f0eecab0761a0ab4252d93d02792c954afe8
Carlos Nava <machinery.e@gmail.com>
2020-09-19 21:02:44 -0400

    clases

--- public/app.js
+++ public/app.js
@@ -15,3 +15,25 @@ form.addEventListener('submit', function (e) {
     console.log("type: " + type.value + " \ntofrom: " + tofrom.value + " \ndetails: " + details.value + " \namount: " + amount.valueAsNumber);
     console.log(amount.valueAsNumber);
 });
+// Clases
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " tiene " + this.amount + "$ para " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('Devante Farrell', 'Non aut dolorem libero veritatis harum quo tempore aut.', 250);
+var invTwo = new Invoice('Kacey Hagenes I', 'Dolor odio ut suscipit qui vel.', 350);
+console.log(invOne);
+console.log(invTwo);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'Kallie Dibbert';
+console.log(invOne);
+console.log('invoices', invoices);
--- src/app.ts
+++ src/app.ts
@@ -20,3 +20,32 @@ form.addEventListener('submit',(e:Event)=>{
   
 })
 
+// Clases
+class Invoice {
+  client:string;
+  details:string;
+  amount:number;
+  constructor(c:string,d:string,a:number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+  format(){
+    return `${this.client} tiene ${this.amount}$ para ${this.details}`
+  }
+}
+
+const invOne = new Invoice('Devante Farrell','Non aut dolorem libero veritatis harum quo tempore aut.',250)
+const invTwo = new Invoice('Kacey Hagenes I','Dolor odio ut suscipit qui vel.',350)
+
+console.log(invOne);
+console.log(invTwo);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client ='Kallie Dibbert'
+console.log(invOne);
+
+console.log('invoices',invoices);
